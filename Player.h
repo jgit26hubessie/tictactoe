c1b2c8dcd3ff533ade5a4d0c65f86d4419d4c5aa
Zakia Jebin <zakiajebin@gmail.com>
2025-04-08 20:20:17 +0600

    updated ver

--- Player.h
+++ Player.h
@@ -1,7 +1,37 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-void playerMove(char* spaces, char player);
-void computerMove(char* spaces, char computer);
+#include <iostream>
+#include <string>
+
+class Player {
+private:
+std :: string name;
+char symbol;
+
+public:
+Player(const std :: string& name, char symbol);
+
+std :: string getName() const;
+char getSymbol() const;
+void getMove(int& row, int& col) const;
+};
+
+Player: :Player(const std: :string& name, char symbol)
+: name(name), symbol(symbol) {}
+
+std :: string Player :: getName() const {
+return name;
+}
+
+char Player :: getSymbol() const {
+return symbol;
+}
+
+void Player :: getMove(int& row, int& col) const {
+std :: cout << name << "(" << symbol << "), enter your move (row and column: 0, 1, or 2):
+std :: cin >> row >> col;
+
+}
 
 #endif
