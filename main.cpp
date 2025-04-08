ef3de8136cd31ea7b1463f4c9030bd33938282fb
Zakia Jebin <zakiajebin@gmail.com>
2025-04-08 20:43:53 +0600

    updated feature

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
 #include<iostream>
-#include "board.h"
-#include "player.h"
-#include "game.h"
+#include "Player.h"
+
+int main(){
+
+Player player1("J", 'O');
+
+std::cout << "Name: " << player1.getName() << std::endl;
+std::cout << "Symbol: " << player1.getSymbol() << std::endl;
+
+int row, column;
+player1.getMove(row, column);
+
+std::cout << "You entered: Row = " << row << ", Column = " << column << std::endl;
+
+return 0;
+}
+
